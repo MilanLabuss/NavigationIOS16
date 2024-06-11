0ff7ac30e3d8c8d341bc9b91fdee43693255acc1
MilanLabuss <milanlabus29@gmail.com>
2024-06-11 10:25:42 +0100

    Initial Commit

---
+++ NavigationIOS16/NavigationIOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationIOS16App.swift
+//  NavigationIOS16
+//
+//  Created by Milan Labus on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationIOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
